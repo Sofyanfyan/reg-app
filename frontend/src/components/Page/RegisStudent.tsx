12046ae058ddaa8a13620117018b243ae5aa358e
Sofyanfyan <fyans665@gmail.com>
2023-12-22 16:00:33 +0700

    add: form student

--- frontend/src/components/Page/RegisStudent.tsx
+++ frontend/src/components/Page/RegisStudent.tsx
@@ -1,10 +1,176 @@
 "use client";
-import { FormEvent } from "react";
+import { FormEvent, useState, ChangeEvent } from "react";
+import { InputText } from "primereact/inputtext";
+import { Calendar } from "primereact/calendar";
+import { Nullable } from "primereact/ts-helpers";
+import {
+  Dropdown,
+  DropdownChangeEvent,
+  DropdownPassThroughMethodOptions,
+} from "primereact/dropdown";
 
 export default function RegisStudent({ ...props }) {
   const { setIdx, setForm } = props;
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  const [dateBirth, setDateBirth] = useState<Nullable<Date>>(null);
+  const [dateExp, setDateExp] = useState<Nullable<Date>>(null);
+  const [student, setStudent] = useState({
+    grade_id: "",
+    is_active: true,
+    name: "",
+    gender: "",
+    religion: "",
+    place_birth: "",
+    date_birth: "",
+    id_or_passport: "",
+    nationality: "",
+    place_of_issue: "",
+    date_exp: "",
+  });
+
+  const [error, setError] = useState({
+    grade_id: "",
+    name: "",
+    gender: "",
+    religion: "",
+    place_birth: "",
+    date_birth: "",
+    id_or_passport: "",
+    nationality: "",
+    place_of_issue: "",
+    date_exp: "",
+  });
+
+  type Options = {
+    name: string;
+  };
+
+  const gender: Options[] = [{ name: "Female" }, { name: "Male" }];
+
+  const religion: Options[] = [
+    { name: "Islam" },
+    { name: "Christianity" },
+    { name: "Catholicism" },
+    { name: "Hinduism" },
+    { name: "Buddhism" },
+    { name: "Confucianism" },
+  ];
+  const grade: Options[] = [
+    { name: "Nursery" },
+    { name: "Toddlers" },
+    { name: "Primary" },
+    { name: "Secondary" },
+    { name: "IGCSE" },
+  ];
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setStudent((prevState) => ({ ...prevState, [name]: value }));
+    setError((prevState) => ({ ...prevState, [name]: "" }));
+  };
+
+  const handleChangeSelect = (event: DropdownChangeEvent) => {
+    const { name, value } = event.target;
+
+    setStudent((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    console.log(dateBirth);
+    const {
+      grade_id,
+      name,
+      gender,
+      religion,
+      place_birth,
+      date_birth,
+      id_or_passport,
+      nationality,
+    } = student;
+    if (
+      !name ||
+      name.length < 5 ||
+      !gender ||
+      !religion ||
+      !grade_id ||
+      !place_birth ||
+      !date_birth ||
+      !id_or_passport ||
+      !nationality
+    ) {
+      if (!name) {
+        setError((prevState) => ({
+          ...prevState,
+          name: "Fullname is required",
+        }));
+      } else if (name.length < 5) {
+        setError((prevState) => ({
+          ...prevState,
+          name: "Fullname min 5 character.",
+        }));
+      }
+
+      if (!gender) {
+        setError((prevState) => ({
+          ...prevState,
+          gender: "Gender is required",
+        }));
+      }
+
+      if (!religion) {
+        setError((prevState) => ({
+          ...prevState,
+          religion: "Religion is required",
+        }));
+      }
+
+      if (!grade_id) {
+        setError((prevState) => ({
+          ...prevState,
+          grade_id: "Grade is required",
+        }));
+      }
+      if (!place_birth) {
+        setError((prevState) => ({
+          ...prevState,
+          place_birth: "Place of birth is required",
+        }));
+      }
+      if (!date_birth) {
+        setError((prevState) => ({
+          ...prevState,
+          date_birth: "Date of birth is required",
+        }));
+      }
+      if (!id_or_passport) {
+        setError((prevState) => ({
+          ...prevState,
+          id_or_passport: "NIK / Passport is required",
+        }));
+      }
+      if (!nationality) {
+        setError((prevState) => ({
+          ...prevState,
+          nationality: "Nationality is required",
+        }));
+      }
+
+      return;
+    }
+
+    setError({
+      grade_id: "",
+      name: "",
+      gender: "",
+      religion: "",
+      place_birth: "",
+      date_birth: "",
+      id_or_passport: "",
+      nationality: "",
+      place_of_issue: "",
+      date_exp: "",
+    });
     console.log("Next");
     setIdx(2);
     setForm("mother");
@@ -12,34 +178,61 @@ export default function RegisStudent({ ...props }) {
 
   return (
     <div className="w-full p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8">
-      <form className="space-y-6" onSubmit={handleSubmit}>
+      <form className="space-y-6" onSubmit={onSubmit}>
         <h5 className="text-xl font-medium text-gray-900">Student form</h5>
         <div className="grid lg:grid-cols-2 gap-4">
           <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900">
+            <label
+              htmlFor="name"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
               Fullname
             </label>
-            <input
-              type="text"
-              name="name"
+
+            <InputText
               id="name"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-              placeholder="Name"
+              name="name"
+              placeholder="Enter Fullname"
+              className={
+                error.name
+                  ? "bg-gray-50 border border-red-300 text-md text-slate-600 rounded-lg block w-full p-3"
+                  : "bg-gray-50 border border-gray-300 text-md text-slate-600 rounded-lg block w-full p-3"
+              }
+              value={student.name}
+              onChange={handleChange}
+              autoComplete="off"
               required
             />
+            {error.name && (
+              <small className="ms-1 text-red-600">{error.name}</small>
+            )}
           </div>
           <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900">
+            <label
+              htmlFor="id_or_passport"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
               NIK/Passport
             </label>
-            <input
-              type="text"
-              name="id_or_passport"
+            <InputText
               id="id_or_passport"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-              placeholder="NIK / Passport"
+              name="id_or_passport"
+              placeholder="Enter NIK/Passport"
+              className={
+                error.id_or_passport
+                  ? "bg-gray-50 border border-red-300 text-md text-slate-600 rounded-lg block w-full p-3"
+                  : "bg-gray-50 border border-gray-300 text-md text-slate-600 rounded-lg block w-full p-3"
+              }
+              value={student.id_or_passport}
+              onChange={handleChange}
+              autoComplete="off"
               required
             />
+            {error.id_or_passport && (
+              <small className="ms-1 text-red-600">
+                {error.id_or_passport}
+              </small>
+            )}
           </div>
         </div>
         <div className="grid lg:grid-cols-3 gap-4">
@@ -47,104 +240,221 @@ export default function RegisStudent({ ...props }) {
             <label className="block mb-2 text-sm font-medium text-gray-900">
               Gender
             </label>
-            <select
-              id="gender"
+
+            <Dropdown
+              value={student.gender}
+              onChange={handleChangeSelect}
+              options={gender}
               name="gender"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-              required
-            >
-              <option value={"Female"}>Female</option>
-              <option value={"Male"}>Male</option>
-            </select>
+              optionLabel="name"
+              placeholder="Select a Gender"
+              className={
+                error.gender
+                  ? "bg-gray-50 border border-red-300 text-gray-900 text-sm rounded-lg w-full"
+                  : "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full"
+              }
+              pt={{
+                root: {
+                  className:
+                    "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full",
+                },
+                item: ({ context }: DropdownPassThroughMethodOptions) => ({
+                  className: context.selected ? "bg-primary" : undefined,
+                }),
+              }}
+            />
+
+            {error.gender && (
+              <small className="ms-1 text-red-600">{error.gender}</small>
+            )}
           </div>
           <div>
             <label className="block mb-2 text-sm font-medium text-gray-900">
               Religion
             </label>
-            <select
-              id="religion"
+            <Dropdown
+              value={student.religion}
+              onChange={handleChangeSelect}
+              options={religion}
               name="religion"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-              required
-            >
-              <option value={"Islam"}>Islam</option>
-              <option value={"Christianity"}>Christianity</option>
-              <option value={"Catholicism"}>Catholicism</option>
-              <option value={"Hinduism"}>Hinduism</option>
-              <option value={"Buddhism"}>Buddhism</option>
-              <option value={"Confucianism"}>Confucianism</option>
-            </select>
+              optionLabel="name"
+              placeholder="Select a Religion"
+              className={
+                error.religion
+                  ? "bg-gray-50 border border-red-300 text-gray-900 text-sm rounded-lg w-full"
+                  : "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full"
+              }
+              pt={{
+                root: {
+                  className:
+                    "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full",
+                },
+                item: ({ context }: DropdownPassThroughMethodOptions) => ({
+                  className: context.selected ? "bg-primary" : undefined,
+                }),
+              }}
+            />
+            {error.religion && (
+              <small className="ms-1 text-red-600">{error.religion}</small>
+            )}
           </div>
           <div>
             <label className="block mb-2 text-sm font-medium text-gray-900">
               Grade
             </label>
-            <select
-              id="grade_id"
+            <Dropdown
+              value={student.grade_id}
+              onChange={handleChangeSelect}
+              options={grade}
+              // rules={{ required: "Grade is required." }}
               name="grade_id"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-              required
-            >
-              <option value={"Nursery"}>Nursery</option>
-              <option value={"Toddlers"}>Toddlers</option>
-              <option value={"Primary"}>Primary</option>
-              <option value={"Secondary"}>Secondary</option>
-              <option value={"IGCSE"}>IGCSE</option>
-            </select>
+              optionLabel="name"
+              placeholder="Select a Grade"
+              className={
+                error.grade_id
+                  ? "bg-gray-50 border border-red-300 text-gray-900 text-sm rounded-lg w-full"
+                  : "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full"
+              }
+              pt={{
+                root: {
+                  className:
+                    "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full",
+                },
+                item: ({ context }: DropdownPassThroughMethodOptions) => ({
+                  className: context.selected ? "bg-primary" : undefined,
+                }),
+              }}
+            />
+            {error.grade_id && (
+              <small className="ms-1 text-red-600">Grade is required</small>
+            )}
           </div>
         </div>
         <div className="grid lg:grid-cols-3 gap-4">
           <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900">
+            <label
+              htmlFor="nationality"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
               Nationality
             </label>
-            <input
-              type="text"
-              name="nationality"
+            <InputText
               id="nationality"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-              placeholder="Nationality"
+              name="nationality"
+              placeholder="Enter Nationality"
+              className={
+                error.nationality
+                  ? "bg-gray-50 border border-red-300 text-md text-slate-600 rounded-lg block w-full p-3"
+                  : "bg-gray-50 border border-gray-300 text-md text-slate-600 rounded-lg block w-full p-3"
+              }
+              value={student.nationality}
+              onChange={handleChange}
+              autoComplete="off"
               required
             />
+            {error.nationality && (
+              <small className="ms-1 text-red-600">
+                Nationality is required
+              </small>
+            )}
           </div>
           <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900">
+            <label
+              htmlFor="place_birth"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
               Place of Birth
             </label>
-            <input
-              type="text"
-              name="place_of_birth"
-              id="place_of_birth"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-              placeholder="Place of birth"
+            <InputText
+              id="place_birth"
+              name="place_birth"
+              placeholder="Enter Place of Birth"
+              className={
+                error.place_birth
+                  ? "bg-gray-50 border border-red-300 text-md text-slate-600 rounded-lg block w-full p-3"
+                  : "bg-gray-50 border border-gray-300 text-md text-slate-600 rounded-lg block w-full p-3"
+              }
+              value={student.place_birth}
+              onChange={handleChange}
+              autoComplete="off"
               required
+            />
+            {error.place_birth && (
+              <small className="ms-1 text-red-600">
+                Place Birth is required
+              </small>
+            )}
+          </div>
+          <div>
+            <label
+              // htmlFor="date_birth"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
+              Date of birth
+            </label>
+            <Calendar
+              id="date_birth"
+              name="date_birth"
+              ariaLabelledBy="date_birth"
+              inputClassName={
+                error.date_birth
+                  ? "w-full bg-gray-50 border border-red-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-3.5"
+                  : "w-full bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-3.5"
+              }
+              style={{ width: "100%" }}
+              value={dateBirth}
+              onChange={(e) => setDateBirth(e.value)}
+              placeholder="Date of birth"
+              dateFormat="dd/mm/yy"
+              locale="en"
+              mask="99/99/9999"
+              showButtonBar
+              required
+            />
+            {error.date_birth && (
+              <small className="ms-1 text-red-600">{error.date_birth}</small>
+            )}
+          </div>
+        </div>
+
+        <div className="grid lg:grid-cols-2 gap-4">
+          <div>
+            <label
+              htmlFor="place_of_issue"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
+              Place of issue ( NIK/Passport )
+            </label>
+            <InputText
+              id="place_of_issue"
+              name="place_of_issue"
+              placeholder="Place of issue"
+              className="bg-gray-50 border border-gray-300 text-md text-slate-600 rounded-lg block w-full p-3"
+              value={student.place_of_issue}
+              onChange={handleChange}
+              autoComplete="off"
             />
           </div>
           <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900">
-              Date of birth
+            <label
+              // htmlFor="date_exp"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
+              Date of expiry ( NIK/Passport )
             </label>
-
-            <div className="relative max-w-sm">
-              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                <svg
-                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-                </svg>
-              </div>
-              <input
-                datepicker
-                datepicker-format="mm/dd/yyyy"
-                type="text"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Select date"
-              />
-            </div>
+            <Calendar
+              name="date_exp"
+              ariaLabelledBy="date_exp"
+              inputClassName="w-full bg-gray-50 border border-gray-300 text-slate-600 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-3.5"
+              style={{ width: "100%" }}
+              value={dateExp}
+              onChange={(e) => setDateExp(e.value)}
+              placeholder="Date of expiry"
+              dateFormat="dd/mm/yy"
+              locale="en"
+              mask="99/99/9999"
+              showButtonBar
+            />
           </div>
         </div>
 
