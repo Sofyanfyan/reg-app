c4afd69fa455095cb0b786004149f0f979838f49
Sofyanfyan <fyans665@gmail.com>
2023-12-23 14:59:42 +0700

    slicing: form mother

--- frontend/src/components/Page/RegisMother.tsx
+++ frontend/src/components/Page/RegisMother.tsx
@@ -1,9 +1,28 @@
 "use client";
 
-import { FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { InputText } from "primereact/inputtext";
+import { Calendar } from "primereact/calendar";
+import { Nullable } from "primereact/ts-helpers";
+import {
+  Dropdown,
+  DropdownChangeEvent,
+  DropdownPassThroughMethodOptions,
+} from "primereact/dropdown";
 
 export default function RegisMother({ ...props }) {
   const { setIdx, setForm } = props;
+  const [dateBirth, setDateBirth] = useState<Nullable<Date>>();
+
+  const religion: string[] = [
+    "Islam",
+    "Christianity",
+    "Catholicism",
+    "Hinduism",
+    "Buddhism",
+    "Confucianism",
+  ];
+
   const [mother, setMother] = useState({
     name: "",
     place_birth: "",
@@ -20,6 +39,37 @@ export default function RegisMother({ ...props }) {
     phone: "",
     email: "",
   });
+
+  const [error, setError] = useState({
+    name: "",
+    place_birth: "",
+    religion: "",
+    date_birth: "",
+    occupation: "",
+    company_name: "",
+    company_address: "",
+    home_address: "",
+    telephone: "",
+    mobilephone: "",
+    id_or_passport: "",
+    nationality: "",
+    phone: "",
+    email: "",
+  });
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    const { value, name } = event.target;
+
+    setMother((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleChangeSelect = (event: DropdownChangeEvent) => {
+    event.preventDefault();
+    const { value, name } = event.target;
+
+    setMother((prevState) => ({ ...prevState, [name]: value }));
+  };
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -44,27 +94,177 @@ export default function RegisMother({ ...props }) {
             <label className="block mb-2 text-sm font-medium text-gray-900">
               Fullname
             </label>
-            <input
-              type="text"
+            <InputText
+              id="name"
               name="name"
-              id="text"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400"
-              placeholder="Name"
+              placeholder="Enter Fullname"
+              className={
+                error.name
+                  ? "bg-gray-50 border border-red-300 text-md text-slate-600 rounded-lg block w-full p-3"
+                  : "bg-gray-50 border border-gray-300 text-md text-slate-600 rounded-lg block w-full p-3"
+              }
+              value={mother.name}
+              onChange={handleChange}
+              autoComplete="off"
               required
             />
+            {error.name && (
+              <small className="ms-1 text-red-600">{error.name}</small>
+            )}
           </div>
           <div>
             <label className="block mb-2 text-sm font-medium text-gray-900">
               NIK/Passport
             </label>
-            <input
-              type="text"
+            <InputText
+              id="id_or_passport"
               name="id_or_passport"
-              id="text"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400"
-              placeholder="NIK / Passport"
+              placeholder="Enter NIK/Passport"
+              className={
+                error.name
+                  ? "bg-gray-50 border border-red-300 text-md text-slate-600 rounded-lg block w-full p-3"
+                  : "bg-gray-50 border border-gray-300 text-md text-slate-600 rounded-lg block w-full p-3"
+              }
+              value={mother.id_or_passport}
+              onChange={handleChange}
+              autoComplete="off"
               required
             />
+            {error.name && (
+              <small className="ms-1 text-red-600">
+                {error.id_or_passport}
+              </small>
+            )}
+          </div>
+        </div>
+        <div className="grid lg:grid-cols-4 gap-4">
+          <div>
+            <label className="block mb-2 text-sm font-medium text-gray-900">
+              Nationality
+            </label>
+            <InputText
+              id="nationality"
+              name="nationality"
+              placeholder="Enter Nationality"
+              className={
+                error.name
+                  ? "bg-gray-50 border border-red-300 text-md text-slate-600 rounded-lg block w-full p-3"
+                  : "bg-gray-50 border border-gray-300 text-md text-slate-600 rounded-lg block w-full p-3"
+              }
+              value={mother.nationality}
+              onChange={handleChange}
+              autoComplete="off"
+              required
+            />
+            {error.name && (
+              <small className="ms-1 text-red-600">{error.nationality}</small>
+            )}
+          </div>
+          <div>
+            <label className="block mb-2 text-sm font-medium text-gray-900">
+              Place of Birth
+            </label>
+            <InputText
+              id="place_birth"
+              name="place_birth"
+              placeholder="Enter place of Birth"
+              className={
+                error.name
+                  ? "bg-gray-50 border border-red-300 text-md text-slate-600 rounded-lg block w-full p-3"
+                  : "bg-gray-50 border border-gray-300 text-md text-slate-600 rounded-lg block w-full p-3"
+              }
+              value={mother.place_birth}
+              onChange={handleChange}
+              autoComplete="off"
+              required
+            />
+            {error.name && (
+              <small className="ms-1 text-red-600">{error.place_birth}</small>
+            )}
+          </div>
+          <div>
+            <label className="block mb-2 text-sm font-medium text-gray-900">
+              Place of Birth
+            </label>
+            <Calendar
+              id="date_birth"
+              name="date_birth"
+              ariaLabelledBy="date_birth"
+              inputClassName={
+                error.date_birth
+                  ? "w-full bg-gray-50 border border-red-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-3.5"
+                  : "w-full bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-3.5"
+              }
+              style={{ width: "100%" }}
+              value={dateBirth}
+              onChange={(e) => setDateBirth(e.value)}
+              placeholder="Date of birth"
+              dateFormat="dd/mm/yy"
+              locale="en"
+              mask="99/99/9999"
+              showButtonBar
+              required
+            />
+            {error.name && (
+              <small className="ms-1 text-red-600">{error.place_birth}</small>
+            )}
+          </div>
+          <div>
+            <label className="block mb-2 text-sm font-medium text-gray-900">
+              Religion
+            </label>
+            <Dropdown
+              value={mother.religion}
+              onChange={handleChangeSelect}
+              options={religion}
+              name="religion"
+              placeholder="Select a Religion"
+              className={
+                error.religion
+                  ? "bg-gray-50 border border-red-300 text-gray-900 text-sm rounded-lg w-full p-1"
+                  : "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full p-1"
+              }
+              pt={{
+                root: {
+                  className:
+                    "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full p-1",
+                },
+                item: ({ context }: DropdownPassThroughMethodOptions) => ({
+                  className: context.selected ? "bg-primary" : undefined,
+                }),
+              }}
+            />
+            {error.religion && (
+              <small className="ms-1 text-red-600">{error.religion}</small>
+            )}
+          </div>
+        </div>
+        <br />
+        <br />
+        <div className="grid lg:grid-cols-2 gap-4">
+          <div>
+            <label className="block mb-2 text-sm font-medium text-gray-900">
+              NIK/Passport
+            </label>
+            <InputText
+              id="id_or_passport"
+              name="id_or_passport"
+              placeholder="Enter NIK/Passport"
+              className={
+                error.name
+                  ? "bg-gray-50 border border-red-300 text-md text-slate-600 rounded-lg block w-full p-3"
+                  : "bg-gray-50 border border-gray-300 text-md text-slate-600 rounded-lg block w-full p-3"
+              }
+              value={mother.id_or_passport}
+              onChange={handleChange}
+              autoComplete="off"
+              required
+            />
+            {error.name && (
+              <small className="ms-1 text-red-600">
+                {error.id_or_passport}
+              </small>
+            )}
           </div>
         </div>
         <div className="flex justify-between">
