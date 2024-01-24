2ab7df804d1f2410db7a06a0841ef1f36a3181e2
Sofyanfyan <fyans665@gmail.com>
2024-01-24 12:00:48 +0700

    add: localStorage clear for action logout"

--- frontend/src/components/Content/Sidebar.tsx
+++ frontend/src/components/Content/Sidebar.tsx
@@ -10,45 +10,39 @@ export default function Sidebar({ ...props }) {
   const dispatch = useDispatch();
 
   const handleLogout = () => {
-    dispatch(actionLogout());
-
-    // const swalWithBootstrapButtons = Swal.mixin({
-    //   customClass: {
-    //     confirmButton:
-    //       "text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center",
-    //     cancelButton:
-    //       "text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600 me-2",
-    //   },
-    //   buttonsStyling: false,
-    // });
-    // swalWithBootstrapButtons
-    //   .fire({
-    //     title: "Are you sure?",
-    //     text: "You'll be redirected to the login page!",
-    //     icon: "warning",
-    //     showCancelButton: true,
-    //     confirmButtonText: "Yes, logout!",
-    //     cancelButtonText: "No, cancel!",
-    //     reverseButtons: true,
-    //   })
-    //   .then((result) => {
-    //     if (result.isConfirmed) {
-    //       swalWithBootstrapButtons.fire({
-    //         title: "Logout",
-    //         text: "Logout success!.",
-    //         icon: "success",
-    //       });
-    //     } else if (
-    //       /* Read more about handling dismissals below */
-    //       result.dismiss === Swal.DismissReason.cancel
-    //     ) {
-    //       swalWithBootstrapButtons.fire({
-    //         title: "Cancelled",
-    //         text: "Your imaginary file is safe :)",
-    //         icon: "error",
-    //       });
-    //     }
-    //   });
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton:
+          "text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center",
+        cancelButton:
+          "text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600 me-2",
+      },
+      buttonsStyling: false,
+    });
+    swalWithBootstrapButtons
+      .fire({
+        title: "Are you sure?",
+        text: "You'll be redirected to the login page!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes, logout!",
+        cancelButtonText: "No, cancel!",
+        reverseButtons: true,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          dispatch(actionLogout());
+          swalWithBootstrapButtons.fire({
+            title: "Logout",
+            text: "Logout success!.",
+            icon: "success",
+          });
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+        }
+      });
   };
 
   return (
