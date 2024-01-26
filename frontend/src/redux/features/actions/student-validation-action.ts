5e629c7dcbbd1c943eae1a5da1643a948b5a8b81
Sofyanfyan <fyans665@gmail.com>
2024-01-26 16:59:34 +0700

    add: axios sync

--- frontend/src/redux/features/actions/student-validation-action.ts
+++ frontend/src/redux/features/actions/student-validation-action.ts
@@ -6,36 +6,33 @@ import {
   validationSuccess,
 } from "../slices/student-validation-slice";
 import { reqStudent } from "@/helpers/request/handleRegister";
-import { RootState } from "@/redux/store";
 
-export const actionValidationStudent: any =
-  (student: IStudent): ThunkAction<void, RootState, null, any> =>
-  async (dispatch: Dispatch<any>) => {
-    try {
-      const { data } = await axios({
-        data: {
-          grade_id: student.grade_id.id,
-          name: student.name,
-          gender: student.gender,
-          religion: student.religion,
-          place_birth: student.place_birth,
-          date_birth: student.date_birth,
-          id_or_passport: student.id_or_passport,
-          nationality: student.nationality,
-          place_of_issue: student.place_of_issue,
-          date_exp: student.date_exp,
-        },
-        method: "POST",
-        url: baseUrl + "/auth/checks/students",
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-        },
-      });
+export const actionValidationStudent: any = (student: IStudent) => {
+  return (dispatch: Dispatch) =>
+    axios({
+      data: {
+        grade_id: student.grade_id.id,
+        name: student.name,
+        gender: student.gender,
+        religion: student.religion,
+        place_birth: student.place_birth,
+        date_birth: student.date_birth,
+        id_or_passport: student.id_or_passport,
+        nationality: student.nationality,
+        place_of_issue: student.place_of_issue,
+        date_exp: student.date_exp,
+      },
+      method: "POST",
+      url: baseUrl + "/auth/checks/students",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+      },
+    }).then((response) => {
+      console.log("masuk");
 
-      dispatch(validationSuccess());
+      console.log(response.data);
+
       reqStudent(student);
-    } catch (error) {
-      console.log("masuk error");
-      dispatch(validationFailure(error));
-    }
-  };
+      dispatch(validationSuccess());
+    });
+};
